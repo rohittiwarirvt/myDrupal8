5bd97d83368455a55a746f87bf0160e61a16cc4a
Rohit Tiwari <rohittiwarirvt1@gmail.com>
2020-03-01 19:16:00 +0000

    a

--- index.php
+++ index.php
@@ -10,9 +10,6 @@
 
 use Drupal\Core\DrupalKernel;
 use Symfony\Component\HttpFoundation\Request;
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 $autoloader = require_once 'autoload.php';
 
